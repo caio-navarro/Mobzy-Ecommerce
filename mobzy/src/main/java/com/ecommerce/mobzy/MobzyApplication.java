81e0104b9cf7663a7ca3b4b78bf5742f390b2d0b
Kauan <kauanbrandao07@gmail.com>
2025-05-09 19:02:46 -0300

    Início

---
+++ mobzy/src/main/java/com/ecommerce/mobzy/MobzyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.mobzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobzyApplication.class, args);
+	}
+
+}
