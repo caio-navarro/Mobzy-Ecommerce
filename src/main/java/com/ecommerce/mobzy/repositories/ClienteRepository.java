dac4a1db9f3b8a35eaf86e67bc1207823d8aaeff
navarro <caionavarro220@gmail.com>
2025-05-09 23:06:27 -0300

    alterações

--- src/main/java/com/ecommerce/mobzy/repositories/ClienteRepository.java
+++ src/main/java/com/ecommerce/mobzy/repositories/ClienteRepository.java
@@ -1,4 +1,9 @@
 package com.ecommerce.mobzy.repositories;
 
-public class ClienteRepository {
+import com.ecommerce.mobzy.models.ClienteModels;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteModels, Integer> {
 }
--- src/main/java/com/ecommerce/mobzy/repositories/EnderecoClienteRepository.java
+++ src/main/java/com/ecommerce/mobzy/repositories/EnderecoClienteRepository.java
@@ -1,9 +1,9 @@
 package com.ecommerce.mobzy.repositories;
 
-import com.ecommerce.mobzy.entities.EnderecoCliente;
+import com.ecommerce.mobzy.models.EnderecoClienteModels;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnderecoClienteRepository extends JpaRepository<EnderecoCliente, Integer> {
+public interface EnderecoClienteRepository extends JpaRepository<EnderecoClienteModels, Integer> {
 }
