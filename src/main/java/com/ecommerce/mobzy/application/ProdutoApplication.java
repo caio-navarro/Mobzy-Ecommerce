ef88fd30f96b1d826f94ab289efe5db9ad224321
Emanuel <lucasemanuelsena@outlook.com>
2025-05-12 19:39:07 -0300

    .

---
+++ src/main/java/com/ecommerce/mobzy/application/ProdutoApplication.java
@@ -0,0 +1,7 @@
+package com.ecommerce.mobzy.application;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProdutoApplication {
+}
---
+++ src/main/java/com/ecommerce/mobzy/controller/ItemCarrinhoController.java
@@ -0,0 +1,4 @@
+package com.ecommerce.mobzy.controller;
+
+public class ItemCarrinhoController {
+}
