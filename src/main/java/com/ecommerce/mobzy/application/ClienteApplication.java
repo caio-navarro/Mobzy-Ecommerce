1a4c018219548ecf0e53ff299712ff4a285d8c89
navarro <caionavarro220@gmail.com>
2025-05-22 13:30:14 -0300

    melhorias

--- src/main/java/com/ecommerce/mobzy/application/ClienteApplication.java
+++ src/main/java/com/ecommerce/mobzy/application/ClienteApplication.java
@@ -1,8 +1,11 @@
 package com.ecommerce.mobzy.application;
 
+import com.ecommerce.mobzy.entities.Cliente;
 import com.ecommerce.mobzy.models.ClienteModels;
 import com.ecommerce.mobzy.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
@@ -12,23 +15,36 @@ public class ClienteApplication {
     @Autowired
     private ClienteRepository clienteRepository;
 
-    public ClienteModels cadastrar(ClienteModels cliente){
-        return clienteRepository.save(cliente);
+    public ResponseEntity<?> cadastrar(ClienteModels clienteModels) {
+        try {
+            Cliente cliente = Cliente.toCliente(clienteModels);
+            cliente.validarDadosObrigatorios();
+
+            clienteRepository.save(clienteModels);
+
+            return ResponseEntity.ok("Cadastrado com sucesso!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Erro no cadastro: " + e.getMessage());
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado: " + e.getMessage());
+        }
     }
 
-    public List listar(){
+    public List listar() {
         return clienteRepository.findAll();
     }
 
-    public ClienteModels buscarPorId(int id){
+    public ClienteModels buscarPorId(int id) {
         return clienteRepository.findById(id).get();
     }
 
-    public void atualizar(ClienteModels cliente){
+    public void atualizar(ClienteModels cliente) {
         clienteRepository.save(cliente);
     }
 
-    public void deletar(int id){
+    public void deletar(int id) {
         clienteRepository.deleteById(id);
     }
 }
--- src/main/java/com/ecommerce/mobzy/controller/ClienteController.java
+++ src/main/java/com/ecommerce/mobzy/controller/ClienteController.java
@@ -1,4 +1,37 @@
 package com.ecommerce.mobzy.controller;
 
+import com.ecommerce.mobzy.application.ClienteApplication;
+import com.ecommerce.mobzy.models.ClienteModels;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cliente")
+@CrossOrigin(origins = "*")
 public class ClienteController {
+
+    @Autowired
+    private ClienteApplication clienteApplication;
+
+    @GetMapping("/listar")
+    public List<ClienteModels> listar(){
+        return clienteApplication.listar();
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ClienteModels cliente){
+        return clienteApplication.cadastrar(cliente);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletar(@PathVariable int id){
+        clienteApplication.deletar(id);
+    }
+
+    @PutMapping("/atualizar")
+    public void atualizar(@RequestBody ClienteModels cliente){
+        clienteApplication.atualizar(cliente);
+    }
 }
