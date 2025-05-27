704352ecfdf301fae8d3e896ec38abd54399e06f
navarro <caionavarro220@gmail.com>
2025-05-27 00:05:25 -0300

    integração

--- Front-End/JS/pg_login_cadastro.js
+++ Front-End/JS/pg_login_cadastro.js
@@ -13,11 +13,14 @@ function switchTab(tab) {
     }
 }
 
+// cadastro
 document.getElementById("register-form").addEventListener("submit", function (event) {
     event.preventDefault();
 
     const nome = document.getElementById("reg-name").value.trim();
     const email = document.getElementById("reg-email").value.trim();
+    const cpf = document.getElementById("reg-cpf").value.trim();
+    const telefone = document.getElementById("reg-telefone").value.trim();
     const senha = document.getElementById("reg-password").value;
     const confirmarSenha = document.getElementById("confirm-password").value;
 
@@ -29,8 +32,18 @@ document.getElementById("register-form").addEventListener("submit", function (ev
     const cadastroData = {
         nome,
         email,
+        cpf,
+        telefone,
         senha
     };
+
+    console.log({
+        nome,
+        email,
+        cpf,
+        telefone,
+        senha
+    });
 
     fetch("http://localhost:8080/cliente/cadastrar", {
         method: "POST",
@@ -54,5 +67,51 @@ document.getElementById("register-form").addEventListener("submit", function (ev
         .catch(error => {
             console.error("Erro ao cadastrar cliente:", error.message);
             alert("Erro: " + error.message);
+        });
+});
+
+// login
+document.getElementById("login-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("password").value;
+
+    const loginData = {
+        email,
+        senha
+    };
+
+    fetch("http://localhost:8080/cliente/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginData)
+    })
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(msg => {
+                    throw new Error(msg);
+                });
+            }
+            return response.json();
+        })
+        .then(cliente => {
+            console.log("Login sucesso:", cliente);
+            const idCliente = cliente.idCliente;
+            console.log("ID do cliente:", idCliente);
+
+            localStorage.setItem('idCliente', idCliente);
+            localStorage.setItem('nomeCliente', cliente.nome);
+            localStorage.setItem('cpfCliente', cliente.cpf);
+            localStorage.setItem('telCliente', cliente.telefone);
+            localStorage.setItem('emailCliente', cliente.email);
+
+            window.location.href = "dashboard_cliente.html";
+        })
+        .catch(error => {
+            console.error("Erro ", error.message);
+            alert(error.message);
         });
 });
